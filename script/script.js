68f00ae2953eb4f698743244bbb16245a3fbb8f7
guhhrlk <128618336+guhhrlk@users.noreply.github.com>
2024-03-14 08:23:46 -0300

    aba5

--- script/script.js
+++ script/script.js
@@ -1,5 +1,22 @@
 function abrir_aba(num){
+  for(let i = 1; i<= 5; i++){
+    let id_btn = "btn"+1;
+    document.getElementById(id_btn).disabled = false;
+  }
 
-alert(num);
-
+  if(num == "1"){
+    document.getElementById("btn1").disabled = true;
+  }
+  if(num == "2"){
+    document.getElementById("btn2").disabled = true;
+  }
+  if(num == "3"){
+    document.getElementById("btn3").disabled = true;
+  }
+  if(num == "4"){
+    document.getElementById("btn4").disabled = true;
+  }
+  if(num == "5"){
+    document.getElementById("btn5").disabled = true;
+  }
 }
