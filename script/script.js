3f96f93eb9f594dceb4476e4993a75e739ed21dc
guhhrlk <128618336+guhhrlk@users.noreply.github.com>
2024-03-14 08:32:46 -0300

    aba7

--- script/script.js
+++ script/script.js
@@ -8,17 +8,22 @@ function abrir_aba(num){
 
   if(num == "1"){
     document.getElementById("btn1").disabled = true;
+    document.getElementById("aba1").style.display = "block";
   }
   if(num == "2"){
     document.getElementById("btn2").disabled = true;
+    document.getElementById("aba2").style.display = "block";
   }
   if(num == "3"){
     document.getElementById("btn3").disabled = true;
+    document.getElementById("aba3").style.display = "block";
   }
   if(num == "4"){
     document.getElementById("btn4").disabled = true;
+    document.getElementById("aba4").style.display = "block";
   }
   if(num == "5"){
     document.getElementById("btn5").disabled = true;
+    document.getElementById("aba5").style.display = "block";
   }
 }
